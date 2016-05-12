54a70a7be882d0834d9b634b09794515063a2945
Pablo Lucas <pablo.lucv@gmail.com>
2016-05-12 18:19:42 -0500

    Fetching Data

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,7 +1,6 @@
 <?php
 
-Route::get('/', 'PagesController@home');
-
-Route::get('about', 'PagesController@about');
+Route::get('cards', 'CardsController@index');
+Route::get('cards/{card}', 'CardsController@show');
 
 
