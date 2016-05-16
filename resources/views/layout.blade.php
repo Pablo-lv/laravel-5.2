b5db9fe0137bbbfe73462ba989e7e7ca20b154ca
Pablo Lucas <pablo.lucv@gmail.com>
2016-05-16 18:42:23 -0500

    Forms

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,8 @@
 <head>
     <title>Laravel</title>
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    {{-- <link rel="stylesheet" href="{{ elixir('css/app.css') }}"> --}}
     @yield('header')
 </head>
 <body>
